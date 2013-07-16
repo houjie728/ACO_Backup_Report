96bc3a69db018e9afdbc8c2111971535f1bdfe7e
Jerry Hou <houjie728@gmail.com>
2013-07-16 14:32:51 +0800

    added onduty info

--- ACO_Backup_Daily_Details_Report.pl
+++ ACO_Backup_Daily_Details_Report.pl
@@ -22,30 +22,44 @@ use Net::SMTP;
 use MIME::Lite;
 
 ####### Variables ########
+my ($CONDITION,$smtp,$buffer,$result,$servername,$backupstatus,$DSN,$dbh,$query,$sth,$i,$dbhost,$siteTotal,$onduty);
+my ($finishTotal,$incompleteTotal,$failedTotal,$otherTotal,$allTotal) = (0,0,0,0,0);
 @ACOserver = ("shabus01","wuxbus01","wxibus10","slfbus10","txqbus10","gotbus01","gotbus02","hacfile01","hcmprt01","sgpbus01","rjnbus01","yanbus01","szhbus01");
 
-# recipients  
-my $to1 = 'Jerry_hou@jabil.com';
-my $to2 = 'jm_liu@jabil.com';
-my $to3 = 'leo_yan@jabil.com';
-my $to4 = 'summy_pu@jabil.com';
-my $to5 = 'tang_tang@jabil.com';
+# Focus on-duty SE
+my $month = strftime '%m', localtime();
+if ($month eq "01" or $month eq "07"){
+	 $onduty = 'leo_yan@jabil.com';
+} elsif ($month eq "02" or $month eq "08") {
+	 $onduty = 'jm_liu@jabil.com';
+} elsif ($month eq "03" or $month eq "09") {
+	 $onduty = 'tai_nguyen@jabil.com';
+} elsif ($month eq "04" or $month eq "10") {
+	 $onduty = 'tang_tang@jabil.com';
+}	elsif ($month eq "05" or $month eq "11") {
+	 $onduty = 'summy_pu@jabil.com';
+}	elsif ($month eq "06" or $month eq "12") {
+	 $onduty = 'atul_gangwal@jabil.com';
+}
+# ACO Managers
+my $to2 = 'jerry_hou@jabil.com';
+my $to3 = 'senghoe_ong@jabil.com';
+
 my $SMTPSERVER = "corimc04.jabil.com";
 my $from = 'ACO_Daily_Backup@jabil.com';
 # Date 86400 means one day
 my $datestring = strftime '%Y/%m/%d', localtime();
-my $subject = "Scheduled report: ACO Daily Backup Status Summary Report - $datestring \n";
+my $subject = "Scheduled report: ACO Daily Backup Status Detail Report - $datestring \n";
 # SQL Query Time Setting
 my $startDate = strftime '%Y%m%d', localtime(time-86400);
 my $endDate = strftime '%Y%m%d', localtime();
 my $startTime = $startDate." 00:00:01";
 my $endTime = $endDate." 13:30:00";
-my ($CONDITION,$smtp,$buffer,$result,$servername,$backupstatus,$DSN,$dbh,$query,$sth,$i,$dbhost,$siteTotal);
-my ($finishTotal,$incompleteTotal,$failedTotal,$otherTotal,$allTotal) = (0,0,0,0,0);
 
 $buffer .="<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n<style TYPE=\"text/css\">\n<!-- body {color: #000000;} table {width:85%;padding: 0;margin: 0;} td {border: 1px solid #C1DAD7; font-size:11px; padding: 6px 6px 6px 12px;  } h3 {font: bold 11px Verdana, Arial, Helvetica, sans-serif; } th{font: bold 11px Verdana, Arial, Helvetica, sans-serif; color: #FFFFFF; border: 1px solid #C1DAD7; letter-spacing: 2px; text-transform: uppercase; text-align: left; padding: 6px 6px 6px 12px; background: #4F81BD;}  .trAll { background: #F3F3F3;  } .trFail { background: #F4CCCC; } .trIncomplete { background: #FFF2CC; } .trFinish { background: #B6D7A8; } -->\n</style>\n</head>\n";
 $buffer .="<body>\n";
 $buffer .= "<i>* This script only check the latest status of daily backup job! If shows \"no data\", please help verify manually!</i>\n";
+$buffer .= "<h3 style=\"font: bold 11px Verdana, Arial, Helvetica, sans-serif;\">On-Duty SE:  $onduty</h3>\n";
 $buffer .= "<h3 style=\"font: bold 11px Verdana, Arial, Helvetica, sans-serif;\">Daily Backup Status: $startDate - $endDate</h3>\n";
 $buffer .= "<table width=\"85%\" cellspacing=\"0\" cellpadding=\"0\" border=\"0\">\n";
 $buffer .= "<tr>\n";
@@ -216,13 +230,13 @@ $buffer .="</body>\n</html>\n";
 ### Email Function ###
 $smtp = Net::SMTP->new($SMTPSERVER) or die print "Couldn't connect to SMTP Server $SMTPSERVER Error $smtp - $!\n";
 $smtp->mail($from);
-$smtp->to($to1);
-#$smtp->cc($to2,$to3,$to4,$to5);
+$smtp->to($onduty);
+#$smtp->cc($to2,$to3);
 
 my $msg = MIME::Lite->new(
         From    => $from,
-        To      => $to1,
-        Cc	=> [$to2,$to3,$to4,$to5], 
+        To      => $onduty,
+        Cc	=> [$to2,$to3], 
         Subject => $subject,
         Type    =>'multipart/related',
         )or print "Error creating MIME body: $!\n";
--- ACO_Backup_Month_Report.pl
+++ ACO_Backup_Month_Report.pl
@@ -22,25 +22,36 @@ use Net::SMTP;
 use MIME::Lite;
 
 ####### Variables ########
+my ($CONDITION,$smtp,$buffer,$result,$servername,$backupstatus,$DSN,$dbh,$query,$sth,$i,$dbhost,$siteTotal,$successTotal,$successRate,$startDate,$endDate,$startTime,$endTime);
+my ($finishTotal,$incompleteTotal,$failedTotal,$otherTotal,$allTotal,$a,$b,$c) = (0,0,0,0,0,0,0,0);
 @ACOserver = ("shabus01","wuxbus01","wxibus10","slfbus10","txqbus10","gotbus01","gotbus02","hacfile01","hcmprt01","sgpbus01","rjnbus01","yanbus01","szhbus01");
 
+# Focus on-duty SE
+my $month = strftime '%m', localtime(time-86400);
+if ($month eq "01" or $month eq "07"){
+	 $onduty = 'leo_yan@jabil.com';
+} elsif ($month eq "02" or $month eq "08") {
+	 $onduty = 'jm_liu@jabil.com';
+} elsif ($month eq "03" or $month eq "09") {
+	 $onduty = 'tai_nguyen@jabil.com';
+} elsif ($month eq "04" or $month eq "10") {
+	 $onduty = 'tang_tang@jabil.com';
+}	elsif ($month eq "05" or $month eq "11") {
+	 $onduty = 'summy_pu@jabil.com';
+}	elsif ($month eq "06" or $month eq "12") {
+	 $onduty = 'atul_gangwal@jabil.com';
+}
 # recipients  
 my $to1 = 'Jerry_hou@jabil.com';
-my $to2 = 'jm_liu@jabil.com';
-my $to3 = 'leo_yan@jabil.com';
-my $to4 = 'summy_pu@jabil.com';
-my $to5 = 'tang_tang@jabil.com';
 my $SMTPSERVER = "corimc04.jabil.com";
 my $from = 'ACO_Daily_Backup@jabil.com';
 
-my ($CONDITION,$smtp,$buffer,$result,$servername,$backupstatus,$DSN,$dbh,$query,$sth,$i,$dbhost,$siteTotal,$successTotal,$successRate,$startDate,$endDate,$startTime,$endTime);
-my ($finishTotal,$incompleteTotal,$failedTotal,$otherTotal,$allTotal,$a,$b,$c) = (0,0,0,0,0,0,0,0);
 # generate a daily backup job status report for last ### days
-my $reportDays = 40; 
+my $reportDays = 30; 
 my $subject = "Scheduled report: ACO Backup Status Summary Report - Last $reportDays days \n";
 $buffer .="<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n<style TYPE=\"text/css\">\n<!-- body {color: #000000;} table {width:85%;padding: 0;margin: 0;} td {border: 1px solid #C1DAD7; font-size:11px; padding: 6px 6px 6px 12px;  } h3 {font: bold 11px Verdana, Arial, Helvetica, sans-serif; } th{font: bold 11px Verdana, Arial, Helvetica, sans-serif; color: #FFFFFF; border: 1px solid #C1DAD7; letter-spacing: 2px; text-transform: uppercase; text-align: left; padding: 6px 6px 6px 12px; background: #4F81BD;}  .trAll { background: #F3F3F3;  } .trFail { background: #F4CCCC; } .trIncomplete { background: #FFF2CC; } .trFinish { background: #B6D7A8; } -->\n</style>\n</head>\n";
 $buffer .="<body>\n";
-
+$buffer .= "<h3 style=\"font: bold 11px Verdana, Arial, Helvetica, sans-serif;\">On-Duty SE ($month):  $onduty</h3>\n";
 $buffer .="<h3 style=\"font: bold 11px Verdana, Arial, Helvetica, sans-serif;\">Status Summary - Last $reportDays days</h3>\n";
 $buffer .="<table width=\"85%\" cellspacing=\"0\" cellpadding=\"0\" border=\"0\">";
 $buffer .="<tr align=\"center\">
@@ -181,12 +192,12 @@ $buffer .="</body>\n</html>\n";
 $smtp = Net::SMTP->new($SMTPSERVER) or die print "Couldn't connect to SMTP Server $SMTPSERVER Error $smtp - $!\n";
 $smtp->mail($from);
 $smtp->to($to1);
-#$smtp->cc($to2,$to3,$to4,$to5);
+$smtp->cc($onduty);
 
 my $msg = MIME::Lite->new(
         From    => $from,
         To      => $to1,
-        Cc	=> [$to2,$to3,$to4,$to5], 
+        Cc			=> $onduty, 
         Subject => $subject,
         Type    =>'multipart/related',
         )or print "Error creating MIME body: $!\n";
