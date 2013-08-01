2b42f1c3355b83e1f358ba11f8a585903fb487c4
Jerry Hou <houjie728@gmail.com>
2013-08-01 13:12:00 +0800

    fine-tune

--- ACO_onduty.pl
+++ ACO_onduty.pl
@@ -69,7 +69,7 @@ my $SMTPSERVER = "corimc04.jabil.com";
 my $from = 'ACO_Daily_Backup@jabil.com';
 
 # focus onduty report for current month
-my $subject = "Scheduled report: ACO Focus On-Duty - $YearMonth \n";
+my $subject = "Scheduled report: Reminder: ACO Focus On-Duty - $YearMonth \n";
 $buffer .="<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n<style TYPE=\"text/css\">\n<!-- body {color: #000000;} table {width:85%;padding: 0;margin: 0;} td {border: 1px solid #C1DAD7; font-size:11px; padding: 6px 6px 6px 12px;  } h3 {font: bold 11px Verdana, Arial, Helvetica, sans-serif; } th{font: bold 11px Verdana, Arial, Helvetica, sans-serif; color: #FFFFFF; border: 1px solid #C1DAD7; letter-spacing: 2px; text-transform: uppercase; text-align: left; padding: 6px 6px 6px 12px; background: #4F81BD;}  .trAll { background: #F3F3F3;  } .trFail { background: #F4CCCC; } .trIncomplete { background: #FFF2CC; } .trFinish { background: #B6D7A8; } -->\n</style>\n</head>\n";
 $buffer .="<body>\n";
 $buffer .= "<h3 style=\"font: bold 11px Verdana, Arial, Helvetica, sans-serif;\">ACO Focus On-Duty - $YearMonth</h3>\n";
